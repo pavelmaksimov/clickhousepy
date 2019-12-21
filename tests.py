f14bdcd60db2276863e6e7bc346fd31b30c808b4
pmaksimov <vur21@ya.ru>
2019-12-21 22:46:38 +0300

    тесты

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import yaml
-
+import datetime as dt
 from clickhousepy import Client
-
+import time
 with open("config.yml", "r") as stream:
     data_loaded = yaml.safe_load(stream)
 
@@ -16,28 +16,32 @@ TEST_TABLE = '__test'
 
 
 def test():
+    client.drop_table(TEST_DB, TEST_TABLE)
     client.create_table_mergetree(
         TEST_DB, TEST_TABLE,
         columns=['s String', 'd DateTime'],
-        orders=['s']
+        orders=['s'],
+        partition=["s"]
     )
     t = client.Table(TEST_DB, TEST_TABLE)
-    t.insert([{"s": "--"}, {"s": "111"}], columns=["s"])
-    print(t.count_rows())
-    print(t.min_date(date_column_name="d"))
+    print(t.describe())
+    t.insert([{"s": "1", "d": dt.datetime(2000,1,1)},
+              {"s": "2", "d": dt.datetime(2000,1,2)},
+              {"s": "3", "d": dt.datetime(2000,1,3)}],
+             columns=["s", "d"])
+    print(t.get_count_rows())
+    print(t.get_min_date(date_column_name="d"))
+    print(t.get_max_date(date_column_name="d"))
+    print(t.get("SELECT * FROM {db}.{table}"))
+    print(t.get_df("SELECT * FROM {db}.{table}"))
+    t.drop_partitions("1")
+    t.drop_partitions([["2"], ["3"]])
+    time.sleep(1)
+    assert t.get_count_rows() == 0
     t.truncate()
-    print(t.count_rows())
-    t.drop()
-    print(t.exists())
-
-
-def test_create_table_mergetree():
-    r = client.create_table_mergetree(TEST_DB, TEST_TABLE, columns=['s String', 'd DateTime'], orders=['s'],
-                                      partition=["s"], sample=["s"], primary_key=["s"], ttl="d", if_not_exists=False)
-    r = client.drop_table(TEST_DB, TEST_TABLE)
-    r = client.create_table_mergetree(TEST_DB, TEST_TABLE, columns=[("s", "String")], orders=["s"], partition=["s"],
-                                      sample=["s"])
-    r = client.drop_table(TEST_DB, TEST_TABLE)
+    assert t.get_count_rows() == 0
+    t.drop_table()
+    assert t.exists() == False
 
 
 def test_optimize_table():
@@ -53,59 +57,35 @@ def test_optimize_table():
     r = client.drop_table(TEST_DB, TEST_TABLE)
 
 
-def test_describe():
-    r = client.describe('direct', 'bids')
-    print(r)
-
-
-def test_get_min_date():
-    r = client.get_min_date('direct', 'bids')
-    print(r)
-
-
-def test_get_max_date():
-    r = client.get_max_date('direct', 'bids')
-    print(r)
-
-
-def test_count_rows():
-    r = client.get_count_rows(TEST_DB, TEST_TABLE)
-    print(f"\n{r}\n")
-
-
-def test_drop_table():
-    r = client.drop_table(TEST_DB, TEST_TABLE)
-    print(r)
-
-
 def test_mutation():
-    client.drop_table(TEST_DB, TEST_TABLE)
+    t = client.Table(TEST_DB, TEST_TABLE)
+    t.drop_table()
     client.create_table_mergetree(TEST_DB, TEST_TABLE, columns=['s String', 'n String'], orders=['s'])
-    client.execute('INSERT INTO {}.{} VALUES'.format(TEST_DB, TEST_TABLE),
-                   [{"s": "--", "n": "6"}, {"s": "111", "n": "0"}])
-    mutation_id = client.update(TEST_DB, TEST_TABLE, update='''  n = '1000'   ''', where='''  n = '6'   ''')
+    t.insert(data=[{"s": "--", "n": "6"}, {"s": "111", "n": "0"}])
+    mutation_id = t.update(update='''  n = '1000'   ''', where='''  n = '6'   ''', prevent_parallel_processes=True, sleep=1)
     print("start mutation")
     is_mutation_done = client.is_mutation_done(mutation_id)
     print('is mutation?', is_mutation_done)
     print(client.execute('SELECT * FROM {}.{} '.format(TEST_DB, TEST_TABLE)))
-    client.drop_table(TEST_DB, TEST_TABLE)
+    t.delete(where="n = '1000'", prevent_parallel_processes=True, sleep=1)
+    print(client.execute('SELECT * FROM {}.{} '.format(TEST_DB, TEST_TABLE)))
+    t.drop_table()
 
 
 def test_insert_transform_from_table():
     test_insert_table = TEST_TABLE + '2'
     client.drop_table(TEST_DB, test_insert_table)
     client.drop_table(TEST_DB, TEST_TABLE)
-    client.create_table_mergetree(TEST_DB, TEST_TABLE, columns=['s String'], orders=['s'])
-    client.create_table_mergetree(TEST_DB, test_insert_table, columns=['s Int32'], orders=['s'])
-    client.execute('INSERT INTO {}.{} VALUES'.format(TEST_DB, TEST_TABLE),
-                   [{"s": "--"}, {"s": "111"}])
-    client.insert_transform_from_table(TEST_DB, TEST_TABLE, TEST_DB, test_insert_table)
-    client.insert_select(TEST_DB, test_insert_table,
-                         query=''' SELECT toInt32OrZero(ifNull(toString(s), '')) FROM to_delete.__test ''')
-    client.insert_select(TEST_DB, test_insert_table, query='SELECT s FROM to_delete.__test')
-    client.insert_select(TEST_DB, test_insert_table, query='SELECT s FROM to_delete.__test', columns='s')
-    client.drop_table(TEST_DB, test_insert_table)
-    client.drop_table(TEST_DB, TEST_TABLE)
+    t = client.create_table_mergetree(TEST_DB, TEST_TABLE, columns=['s String'], orders=['s'])
+    t2 = client.create_table_mergetree(TEST_DB, test_insert_table, columns=['s Int32'], orders=['s'])
+    t.insert([{"s": "--"}, {"s": "111"}])
+    t2.insert_transform_from_table(TEST_DB, test_insert_table)
+    print(str(t))
+    t2.insert_select(query=''' SELECT toInt32OrZero(ifNull(toString(s), '')) FROM {}.{} '''.format(t.db, t.table))
+    t2.insert_select(query='SELECT s FROM {}.{}'.format(t.db, t.table))
+    t2.insert_select(query='SELECT s FROM {}.{}'.format(t.db, t.table), columns='s')
+    t.drop_table()
+    t2.drop_table()
 
 
 def test_get_df():
