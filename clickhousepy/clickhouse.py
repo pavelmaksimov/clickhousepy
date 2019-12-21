0264c0f6b915070d74d348bde05157de8c3417e6
pmaksimov <vur21@ya.ru>
2019-12-21 19:46:45 +0300

    добавлены kwargs через которые, можно пробросить параметры доступные драйвером clickhouse_driver

--- clickhousepy/clickhouse.py
+++ clickhousepy/clickhouse.py
@@ -11,22 +11,25 @@ class Client(ChClient):
     def Table(self, db, table):
         return Table(self, db, table)
 
-    def truncate(self, db, table):
-        return self.execute("TRUNCATE TABLE {}.{}".format(db, table))
+    def truncate(self, db, table, **kwargs):
+        return self.execute("TRUNCATE TABLE {}.{}".format(db, table), **kwargs)
 
-    def exists(self, db, table):
-        return bool(self.execute("EXISTS TABLE {}.{}".format(db, table))[0][0])
-
-    def describe(self, db, table):
-        return self.execute("DESCRIBE TABLE {}.{}".format(db, table))
-
-    def rename(self, db, table, new_db, new_table):
-        return self.execute(
-            """RENAME TABLE {}.{} TO {}.{}""".format(db, table, new_db, new_table)
+    def exists(self, db, table, **kwargs):
+        return bool(
+            self.execute("EXISTS TABLE {}.{}".format(db, table), **kwargs)[0][0]
         )
 
-    def create_db(self, db):
-        return self.execute("CREATE DATABASE IF NOT EXISTS {}".format(db))
+    def describe(self, db, table, **kwargs):
+        return self.execute("DESCRIBE TABLE {}.{}".format(db, table), **kwargs)
+
+    def rename(self, db, table, new_db, new_table, **kwargs):
+        return self.execute(
+            """RENAME TABLE {}.{} TO {}.{}""".format(db, table, new_db, new_table),
+            **kwargs,
+        )
+
+    def create_db(self, db, **kwargs):
+        return self.execute("CREATE DATABASE IF NOT EXISTS {}".format(db), **kwargs)
 
     def create_table_mergetree(
         self,
@@ -42,6 +45,7 @@ class Client(ChClient):
         extra_before_settings="",
         engine="MergeTree",
         settings=None,
+        **kwargs,
     ):
         """
 
@@ -105,7 +109,7 @@ class Client(ChClient):
             extra=extra_before_settings,
             engine=engine,
         )
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
     def create_table_log(
         self,
@@ -115,6 +119,7 @@ class Client(ChClient):
         if_not_exists=True,
         temporary=False,
         type_log_table="StripeLog",
+        **kwargs,
     ):
         """
 
@@ -144,21 +149,21 @@ class Client(ChClient):
             table=table,
             type_log_table=type_log_table,
         )
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def copy_table(self, db, table, new_db, new_table):
+    def copy_table(self, db, table, new_db, new_table, **kwargs):
         query = "CREATE TABLE {}.{} as {}.{}".format(new_db, new_table, db, table)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def drop_db(self, db, if_exists=True):
+    def drop_db(self, db, if_exists=True, **kwargs):
         exists = "IF EXISTS" if if_exists else ""
-        return self.execute("DROP DATABASE {} {}".format(exists, db))
+        return self.execute("DROP DATABASE {} {}".format(exists, db), **kwargs)
 
-    def drop_table(self, db, table, if_exists=True):
+    def drop_table(self, db, table, if_exists=True, **kwargs):
         exists = "IF EXISTS" if if_exists else ""
-        return self.execute("DROP TABLE {} {}.{}".format(exists, db, table))
+        return self.execute("DROP TABLE {} {}.{}".format(exists, db, table), **kwargs)
 
-    def drop_partitions(self, db, table, partitions):
+    def drop_partitions(self, db, table, partitions, **kwargs):
         """
         :param partitions: str or int or list(list)
              Если ключ таблицы состоит из одного столбца, то можно передать, как str
@@ -176,57 +181,66 @@ class Client(ChClient):
             query = "ALTER TABLE {}.{} DROP PARTITION ({})".format(
                 db, table, partition_key
             )
-            self.execute(query)
+            self.execute(query, **kwargs)
 
         if not isinstance(partitions, list):
             partitions = [[str(partitions)]]
 
         map(_drop_partition, partitions)
 
-    def is_mutation_done(self, mutation_id):
+    def is_mutation_done(self, mutation_id, **kwargs):
         query = "SELECT is_done " "FROM system.mutations " "WHERE mutation_id='{}' "
         query = query.format(mutation_id)
-        r = self.execute(query)
+        r = self.execute(query, **kwargs)
         return r[0][0] if r else None
 
-    def _get_last_mutation_id(self, db, table, command):
+    def _get_last_mutation_id(self, db, table, command, **kwargs):
         command = command.replace("'", "\\'")
-        command = command[command.upper().find("UPDATE"):]
+        command = command[command.upper().find("UPDATE") :]
         query = (
             "SELECT mutation_id "
             "FROM system.mutations "
             """WHERE database='{}' AND table='{}' AND command='{}' """
             "ORDER BY create_time DESC"
         ).format(db, table, command)
-        r = self.execute(query)
+        r = self.execute(query, **kwargs)
         return r[0][0] if r else None
 
-    def _count_run_mutations(self, db, table):
+    def _count_run_mutations(self, db, table, **kwargs):
         query = (
             "SELECT count() "
             "FROM system.mutations "
             "WHERE database='{}' AND table='{}' AND is_done=0"
         ).format(db, table)
-        r = self.execute(query)
+        r = self.execute(query, **kwargs)
         return r[0][0]
 
-    def delete(self, db, table, where, prevent_parallel_processes=False, sleep=1):
+    def delete(
+        self, db, table, where, prevent_parallel_processes=False, sleep=1, **kwargs
+    ):
         query = "ALTER TABLE {}.{} DELETE WHERE {}".format(db, table, where)
 
         if prevent_parallel_processes:
             while True:
                 r = self._count_run_mutations(db, table)
                 if r == 0:
-                    self.execute(query)
+                    self.execute(query, **kwargs)
                     return self._get_last_mutation_id(db, table, query)
                 else:
                     time.sleep(sleep)
         else:
-            self.execute(query)
+            self.execute(query, **kwargs)
             return self._get_last_mutation_id(db, table, query)
 
     def update(
-        self, db, table, update, where, prevent_parallel_processes=False, sleep=1
+        self,
+        db,
+        table,
+        update,
+        where,
+        prevent_parallel_processes=False,
+        sleep=1,
+        **kwargs,
     ):
         query = """ALTER TABLE {db}.{t} UPDATE {update} WHERE {where}"""
         query = query.format(db=db, t=table, update=update, where=where)
@@ -235,74 +249,74 @@ class Client(ChClient):
             while True:
                 r = self._count_run_mutations(db, table)
                 if r == 0:
-                    self.execute(query)
+                    self.execute(query, **kwargs)
                     return self._get_last_mutation_id(db, table, query)
                 else:
                     time.sleep(sleep)
         else:
-            self.execute(query)
+            self.execute(query, **kwargs)
             return self._get_last_mutation_id(db, table, query)
 
-    def get_min_date(self, db, table, where=None, date_column_name="Date"):
+    def get_min_date(self, db, table, where=None, date_column_name="Date", **kwargs):
         where = "WHERE " + where if where else ""
         query = "SELECT min({}) FROM {}.{} {}".format(
             date_column_name, db, table, where
         )
-        return self.execute(query)[0][0]
+        return self.execute(query, **kwargs)[0][0]
 
-    def get_max_date(self, db, table, where=None, date_column_name="Date"):
+    def get_max_date(self, db, table, where=None, date_column_name="Date", **kwargs):
         where = "WHERE " + where if where else ""
         query = "SELECT max({}) FROM {}.{} {}".format(
             date_column_name, db, table, where
         )
-        return self.execute(query)[0][0]
+        return self.execute(query, **kwargs)[0][0]
 
-    def get_count_rows(self, db, table, where=None):
+    def get_count_rows(self, db, table, where=None, **kwargs):
         where = "WHERE " + where if where else ""
         query = "SELECT count() FROM {}.{} {}".format(db, table, where)
-        return self.execute(query)[0][0]
+        return self.execute(query, **kwargs)[0][0]
 
-    def optimize_table(self, db, table):
+    def optimize_table(self, db, table, **kwargs):
         query = "OPTIMIZE TABLE {}.{}".format(db, table)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def reload_dictionary(self, dictionary_name):
+    def reload_dictionary(self, dictionary_name, **kwargs):
         query = "RELOAD DICTIONARY {}".format(dictionary_name)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def reload_dictionaries(self):
-        return self.execute("SYSTEM RELOAD DICTIONARIES")
+    def reload_dictionaries(self, **kwargs):
+        return self.execute("SYSTEM RELOAD DICTIONARIES", **kwargs)
 
-    def check_table(self, db, table):
+    def check_table(self, db, table, **kwargs):
         query = "CHECK TABLE {}.{}".format(db, table)
-        return self.execute(query)[0][0]
+        return self.execute(query, **kwargs)[0][0]
 
-    def attach(self, db, table, if_exists=True, cluster=None):
+    def attach(self, db, table, if_exists=True, cluster=None, **kwargs):
         exists = "IF EXISTS" if if_exists else ""
         cluster = "ON CLUSTER {}".format(cluster) if cluster else ""
         query = "ATTACH TABLE {} {}.{} {}".format(exists, db, table, cluster)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def detach(self, db, table, if_exists=True, cluster=None):
+    def detach(self, db, table, if_exists=True, cluster=None, **kwargs):
         exists = "IF EXISTS" if if_exists else ""
         cluster = "ON CLUSTER {}".format(cluster) if cluster else ""
         query = "DETACH TABLE {} {}.{} {}".format(exists, db, table, cluster)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
-    def show_databases(self):
-        return self.execute("SHOW DATABASES")
+    def show_databases(self, **kwargs):
+        return self.execute("SHOW DATABASES", **kwargs)
 
-    def show_tables(self, db=None, like=None):
+    def show_tables(self, db=None, like=None, **kwargs):
         db = "FROM {}".format(db) if db else ""
         like = "LIKE '{}'" if like else ""
-        return self.execute("SHOW TABLES {} {}".format(db, like))
+        return self.execute("SHOW TABLES {} {}".format(db, like), **kwargs)
 
-    def show_process(self):
-        return self.execute("SHOW PROCESSLIST")
+    def show_process(self, **kwargs):
+        return self.execute("SHOW PROCESSLIST", **kwargs)
 
-    def show_create_table(self, db, table):
+    def show_create_table(self, db, table, **kwargs):
         query = "SHOW CREATE TABLE {}.{}".format(db, table)
-        return self.execute(query)[0][0]
+        return self.execute(query, **kwargs)[0][0]
 
     def _transform_data_type_sql(self, name, data_type):
         if data_type.find("Array") > -1:
@@ -317,7 +331,9 @@ class Client(ChClient):
         else:
             return name
 
-    def insert_transform_from_table(self, from_db, from_table, to_db, to_table):
+    def insert_transform_from_table(
+        self, from_db, from_table, to_db, to_table, **kwargs
+    ):
         column_data = self.describe(to_db, to_table)
         columns_list = []
         for i in column_data:
@@ -328,17 +344,17 @@ class Client(ChClient):
         columns_str = ",\n".join(columns_list)
         sql = "INSERT INTO {}.{} SELECT {} FROM {}.{}"
         sql = sql.format(to_db, to_table, columns_str, from_db, from_table)
-        return self.execute(sql)
+        return self.execute(sql, **kwargs)
 
-    def insert(self, db, table, data, columns=None):
+    def insert(self, db, table, data, columns=None, **kwargs):
         columns_str = "({})".format(",".join(columns)) if columns else ""
         query = "INSERT INTO {}.{} {} VALUES".format(db, table, columns_str)
-        return self.execute(query, data)
+        return self.execute(query, data, **kwargs)
 
-    def insert_select(self, db, table, query, columns=None):
+    def insert_select(self, db, table, query, columns=None, **kwargs):
         columns_str = "({})".format(",".join(columns)) if columns else ""
         query = "INSERT INTO {}.{} {} {}".format(db, table, columns_str, query)
-        return self.execute(query)
+        return self.execute(query, **kwargs)
 
     def get_df(self, query, columns_names=None, **kwargs):
         result = self.execute(query, **kwargs) or [[]]
@@ -355,71 +371,77 @@ class Table:
     def get_df(self, query, columns_names=None, **kwargs):
         return self._client.get_df(query, columns_names, **kwargs)
 
-    def insert(self, data, columns=None):
-        return self._client.insert(self.db, self.table, data, columns)
+    def insert(self, data, columns=None, **kwargs):
+        return self._client.insert(self.db, self.table, data, columns, **kwargs)
 
-    def insert_select(self, query, columns=None):
-        return self._client.insert_select(self.db, self.table, query, columns)
+    def insert_select(self, query, columns=None, **kwargs):
+        return self._client.insert_select(self.db, self.table, query, columns, **kwargs)
 
-    def insert_transform_from_table(self, from_db, from_table):
+    def insert_transform_from_table(self, from_db, from_table, **kwargs):
         return self._client.insert_transform_from_table(
-            from_db, from_table, self.db, self.table
+            from_db, from_table, self.db, self.table, **kwargs
         )
 
-    def exists(self):
-        return self._client.exists(self.db, self.table)
+    def exists(self, **kwargs):
+        return self._client.exists(self.db, self.table, **kwargs)
 
-    def rename(self, new_db, new_table):
-        return self._client.rename(self.db, self.table, new_db, new_table)
+    def rename(self, new_db, new_table, **kwargs):
+        return self._client.rename(self.db, self.table, new_db, new_table, **kwargs)
 
-    def truncate(self):
-        return self._client.truncate(self.db, self.table)
+    def truncate(self, **kwargs):
+        return self._client.truncate(self.db, self.table, **kwargs)
 
-    def describe(self):
-        return self._client.describe(self.db, self.table)
+    def describe(self, **kwargs):
+        return self._client.describe(self.db, self.table, **kwargs)
 
-    def drop(self):
-        return self._client.drop_table(self.db, self.table)
+    def drop(self, **kwargs):
+        return self._client.drop_table(self.db, self.table, **kwargs)
 
-    def delete(self, where, prevent_parallel_processes=False):
+    def delete(self, where, prevent_parallel_processes=False, **kwargs):
         return self._client.delete(
             self.db,
             self.table,
             where,
             prevent_parallel_processes=prevent_parallel_processes,
+            **kwargs,
         )
 
-    def update(self, where, prevent_parallel_processes=False):
+    def update(self, where, prevent_parallel_processes=False, **kwargs):
         return self._client.update(
             self.db,
             self.table,
             where,
             prevent_parallel_processes=prevent_parallel_processes,
+            **kwargs,
         )
 
-    def count_rows(self, where=None):
-        return self._client.get_count_rows(self.db, self.table, where=where)
+    def count_rows(self, where=None, **kwargs):
+        return self._client.get_count_rows(self.db, self.table, where=where, **kwargs)
 
-    def min_date(self, where=None, date_column_name="Date"):
-        return self._client.get_min_date(self.db, self.table, where, date_column_name)
+    def min_date(self, where=None, date_column_name="Date", **kwargs):
+        return self._client.get_min_date(
+            self.db, self.table, where, date_column_name, **kwargs
+        )
 
-    def max_date(self, where=None, date_column_name="Date"):
-        return self._client.get_max_date(self.db, self.table, where, date_column_name)
+    def max_date(self, where=None, date_column_name="Date", **kwargs):
+        return self._client.get_max_date(
+            self.db, self.table, where, date_column_name, **kwargs
+        )
 
-    def copy(self, new_db, new_table):
-        return self._client.copy_table(self.db, self.table, new_db, new_table)
+    def copy(self, new_db, new_table, **kwargs):
+        return self._client.copy_table(self.db, self.table, new_db, new_table, **kwargs)
 
-    def optimize_table(self):
-        return self._client.optimize_table(self.db, self.table)
+    def optimize_table(self, **kwargs):
+        return self._client.optimize_table(self.db, self.table, **kwargs)
 
-    def check_table(self):
-        return self._client.check_table(self.db, self.table)
+    def check_table(self, **kwargs):
+        return self._client.check_table(self.db, self.table, **kwargs)
 
-    def attach(self, if_exists=True, cluster=None):
-        return self._client.attach(self.db, self.table, if_exists, cluster)
+    def attach(self, if_exists=True, cluster=None, **kwargs):
+        return self._client.attach(self.db, self.table, if_exists, cluster, **kwargs)
 
-    def detach(self, if_exists=True, cluster=None):
-        return self._client.detach(self.db, self.table, if_exists, cluster)
+    def detach(self, if_exists=True, cluster=None, **kwargs):
+        return self._client.detach(self.db, self.table, if_exists, cluster, **kwargs)
 
-    def show_create_table(self):
-        return self._client.show_create_table(self.db, self.table)
+    def show_create_table(self, **kwargs):
+        return self._client.show_create_table(self.db, self.table, **kwargs)
